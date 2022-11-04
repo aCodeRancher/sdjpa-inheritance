e56873fb487c6e990a692d5b7aafe7416c4c41a8
helen <hhma@alumni.cpp.edu>
2022-11-04 18:15:57 -0400

    add examples to add Car and Truck.

--- src/main/java/guru/springframework/sdjpainheritence/domain/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/sdjpainheritence/domain/bootstrap/Bootstrap.java
@@ -1,7 +1,12 @@
 package guru.springframework.sdjpainheritence.domain.bootstrap;
 
 import guru.springframework.sdjpainheritence.domain.joined.ElectricGuitar;
+import guru.springframework.sdjpainheritence.domain.repositories.CarRepository;
 import guru.springframework.sdjpainheritence.domain.repositories.ElectricGuitarRepository;
+import guru.springframework.sdjpainheritence.domain.repositories.TruckRepository;
+import guru.springframework.sdjpainheritence.domain.singletable.Car;
+import guru.springframework.sdjpainheritence.domain.singletable.Truck;
+import guru.springframework.sdjpainheritence.domain.singletable.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,11 +20,25 @@ public class Bootstrap implements CommandLineRunner {
     @Autowired
     ElectricGuitarRepository electricGuitarRepository;
 
+    @Autowired
+    CarRepository carRepository;
+
+    @Autowired
+    TruckRepository truckRepository;
+
     @Override
     public void run(String... args) throws Exception {
         ElectricGuitar eg = new ElectricGuitar();
         eg.setNumberOfStrings(6);
         eg.setNumberOfPickups(2);
         electricGuitarRepository.save(eg);
+
+        Car  car = new Car();
+        car.setTrimLevel("trimLevel1");
+        carRepository.save(car);
+
+        Truck truck = new Truck();
+        truck.setPayload(1);
+        truckRepository.save(truck);
     }
 }
